16fba39da388b57d03650ea1b94054f23f073d5c
Lance Bierke <lancebierke@gmail.com>
2021-07-29 23:32:35 -0700

    Acitivty Indicator and Redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -18,6 +18,20 @@ import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 import { Icon } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { connect } from "react-redux";
+import {
+  fetchCampsites,
+  fetchComments,
+  fetchPromotions,
+  fetchPartners,
+} from "../redux/ActionCreators";
+
+const mapDispatchToProps = {
+  fetchCampsites,
+  fetchComments,
+  fetchPromotions,
+  fetchPartners,
+};
 
 const AboutNavigator = createStackNavigator(
   {
@@ -196,13 +210,20 @@ const MainNavigator = createDrawerNavigator(
   },
   {
     drawerBackgroundColor: "#CEC8FF",
-    contentComponent: CustomDrawerContentComponent
+    contentComponent: CustomDrawerContentComponent,
   }
 );
 
 const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
+  componentDidMount() {
+    this.props.fetchCampsites();
+    this.props.fetchComments();
+    this.props.fetchPartners();
+    this.props.fetchPromotions();
+  }
+
   render() {
     return (
       <View
@@ -246,4 +267,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
