1d29514daa773ca52b1c9cbae93e0dadccc7d9e7
Lance Bierke <lancebierke@gmail.com>
2021-08-03 23:54:46 -0700

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,10 +1,10 @@
-import { threshold } from "jimp";
 import React, { Component } from "react";
 import { FlatList, ScrollView, Text } from "react-native";
 import { Card, ListItem } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import Loading from "./LoadingComponent";
+import * as Animatable from "react-native-animatable";
 
 const mapStateToProps = (state) => {
   return {
@@ -56,15 +56,18 @@ class About extends Component {
     if (this.props.partners.errMess) {
       return (
         <ScrollView>
+          <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
           <Mission />
           <Card title="Community Partners">
             <Text>{this.props.partners.errMess}</Text>
           </Card>
+          </Animatable.View>
         </ScrollView>
       );
     }
     return (
       <ScrollView>
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
         <Mission />
         <Card title="Community Partners">
           <FlatList
@@ -73,6 +76,7 @@ class About extends Component {
             keyExtractor={(item) => item.id.toString()}
           ></FlatList>
         </Card>
+        </Animatable.View>
       </ScrollView>
     );
   }
--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -12,6 +12,7 @@ import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import { postFavorite, postComment } from "../redux/ActionCreators";
+import * as Animatable from "react-native-animatable";
 
 const mapStateToProps = (state) => {
   return {
@@ -32,34 +33,36 @@ function RenderCampsite(props) {
 
   if (campsite) {
     return (
-      <Card
-        featuredTitle={campsite.name}
-        image={{ uri: baseUrl + campsite.image }}
-      >
-        <Text style={{ margin: 10 }}>{campsite.description}</Text>
-        <View style={styles.cardRow}>
-          <Icon
-            name={props.favorite ? "heart" : "heart-o"}
-            type="font-awesome"
-            color="#f50"
-            raised
-            reverse
-            onPress={() =>
-              props.favorite
-                ? console.log("Already set as a favorite")
-                : props.markFavorite()
-            }
-          />
-          <Icon
-            name="pencil"
-            type="font-awesome"
-            color="#5637DD"
-            raised
-            reverse
-            onPress={() => props.onShowModal()}
-          />
-        </View>
-      </Card>
+      <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+        <Card
+          featuredTitle={campsite.name}
+          image={{ uri: baseUrl + campsite.image }}
+        >
+          <Text style={{ margin: 10 }}>{campsite.description}</Text>
+          <View style={styles.cardRow}>
+            <Icon
+              name={props.favorite ? "heart" : "heart-o"}
+              type="font-awesome"
+              color="#f50"
+              raised
+              reverse
+              onPress={() =>
+                props.favorite
+                  ? console.log("Already set as a favorite")
+                  : props.markFavorite()
+              }
+            />
+            <Icon
+              name="pencil"
+              type="font-awesome"
+              color="#5637DD"
+              raised
+              reverse
+              onPress={() => props.onShowModal()}
+            />
+          </View>
+        </Card>
+      </Animatable.View>
     );
   }
   return <View />;
@@ -91,6 +94,7 @@ function RenderComments({ comments }) {
   };
 
   return (
+    <Animatable.View animation="fadeInUp" duration={2000} delay={1000}>
     <Card title="Comments">
       <FlatList
         data={comments}
@@ -98,6 +102,7 @@ function RenderComments({ comments }) {
         keyExtractor={(item) => item.id.toString()}
       />
     </Card>
+    </Animatable.View>
   );
 }
 
